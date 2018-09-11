8d8f35c42834575f2d950e9171d21cff78fec5f5
Roger <roger.neil.davenport@gmail.com>
2018-09-11 13:58:57 -0700

    finalised state for estimate form

--- src/estimate.js
+++ src/estimate.js
@@ -1,39 +1,72 @@
 import React, { Component } from "react";
  
 class Estimate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      zip: '',
+      service: 'biMonthly',
+      bed: '',
+      bath: '',
+      common: '',
+
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    console.log('your name is' + this.state.name);
+    e.preventDefault();
+  }
+  handleInputChange(e) {
+    console.log('target', e.target.value);
+    console.log('target name', e.target.name);
+    console.log('state service before setstate', this.state.service);
+    this.setState({[e.target.name]: e.target.value});
+    console.log('state service after setstate', this.state.service);
+    
+    
+
+  }
   render() {
     return (
       <div>
         <h2>Request for Estimate</h2>
         
-        <form className="estimateForm"> 
+        <form className="estimateForm" onSubmit={this.handleSubmit}> 
           <label>
             Name:
-            <input type="text" name="name" />
+            <input type="text" name="name" 
+          onChange={this.handleInputChange} value={this.state.name}/>
           </label>
           <label>
             Email
-            <input type="text" name="name" />
+            <input type="text" name="email" onChange={this.handleInputChange} value={this.state.email}/>
           </label>
           <label>
             Phone:
-            <input type="text" name="name" />
+            <input type="text" name="phone" onChange={this.handleInputChange} value={this.state.phone} />
           </label>
           <label>
             Zip Code:
-            <input type="text" name="name" />
+            <input type="text" name="zip" onChange={this.handleInputChange} value={this.state.zip} />
           </label>
           <label>
             Service Required 
-            <select>
-              <option value="biMonthky">Bi Monthly</option>
-              <option value="monthly">Monthly</option>
-              <option value="weekly">Weekly</option>
-              <option value="move">Move In / Move Out</option>
-              <option value="bnb">B 'n B</option>
-              <option value="deep">Deep Clean</option>
-              <option value="alacarte">A La Carte</option>
-              <option value="offie">Office / Commercial</option>
+            <select name="service" onChange={this.handleInputChange} service={this.state.service}>
+              <option service="biMonthly">Bi Monthly</option>
+              <option service="monthly">Monthly</option>
+              <option service="weekly">Weekly</option>
+              <option service="move">Move In / Move Out</option>
+              <option service="bnb">B 'n B</option>
+              <option service="deep">Deep Clean</option>
+              <option service="alacarte">A La Carte</option>
+              <option service="offie">Office / Commercial</option>
 
             </select>
           </label>
@@ -67,7 +100,7 @@ class Estimate extends Component {
               <option value="4">4</option>
             </select>
           </label>
-
+          <input type="submit" value="Submit" />
         </form>
         
       </div>
