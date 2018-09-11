9aae80150645f12503767ca5579fac7dd3408d5c
Roger <roger.neil.davenport@gmail.com>
2018-09-11 09:40:55 -0700

    added carousel component

--- src/gallery.js
+++ src/gallery.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
+import Carousel from "./carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
  
 class Gallery extends Component {
   render() {
     return (
       <div>
         <h2>Gallery</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          
-        </ol>
+        <Carousel />
       </div>
     );
   }
