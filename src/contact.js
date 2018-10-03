2d4befe9c7dca7e74019d97a51d868040705f882
Roger <roger.neil.davenport@gmail.com>
2018-10-03 13:45:45 -0700

    form submitted now appears after submit button clicked

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     return (
+      
       <div>
         <h2>Contact</h2>
         <p>Please feel free to contact me via email or phone</p>
--- src/estimateHome.js
+++ src/estimateHome.js
@@ -18,6 +18,7 @@ class EstimateHome extends Component {
       bed: '',
       bath: '',
       common: '',
+      submitted: false,
       date: new Date(),
 
     };
@@ -27,35 +28,63 @@ class EstimateHome extends Component {
   }
 
   handleSubmit(e) {
-    console.log('the time of your request is  :' + this.state.date)
-    console.log('your name is' + this.state.name);
-    console.log('your email is' + this.state.email);
-    console.log('your phone is' + this.state.phone);
-    console.log('your zip is' + this.state.zip);
-    console.log('the service you requested is ' + this.state.service);
-    console.log('your beds are ' + this.state.bed);
-    console.log('your baths are' + this.state.bath);
-    console.log('your commons are' + this.state.common);
+    // console.log('the time of your request is  :' + this.state.date)
+    // console.log('your name is' + this.state.name);
+    // console.log('your email is' + this.state.email);
+    // console.log('your phone is' + this.state.phone);
+    // console.log('your zip is' + this.state.zip);
+    // console.log('the service you requested is ' + this.state.service);
+    // console.log('your beds are ' + this.state.bed);
+    // console.log('your baths are' + this.state.bath);
+    // console.log('your commons are' + this.state.common);
     e.preventDefault();
+    console.log('name', this.state);
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      zip: '',
+      service: 'none',
+      bed: '',
+      bath: '',
+      submitted: true,
+      common: '',
+      date: '',
+    });
+   console.log('after setstate in handle sumbit set to blank');
   }
   handleInputChange(e) {
     
     this.setState({[e.target.name]: e.target.value});
    console.log('state changed');
-   console.log('new state', e.target.service);
-    
+   
 
   }
   render() {
+    
+    const isSubmitted = this.state.submitted;
+    console.log('isSubmitted', isSubmitted)
     return (
       <div id="esContainer">
         <h2>Request an Estimate</h2>
         <div id="estimateForm" >
-         <EstimateForm onFormChange={this.handleInputChange} onFormSubmit={this.handleSubmit}/>
+        {!isSubmitted ? (
+          <EstimateForm onFormChange={this.handleInputChange} onFormSubmit={this.handleSubmit}/>
+
+        ) : (
+          <h4>Thank you for your enquiry, you should hear from us within 2 business days</h4>
+        )}
 
         </div>
         <div id="estimateDetail" className="inline">
-        <EstimateDetail service={this.state.service}/>
+        {!isSubmitted ? (
+          <EstimateDetail service={this.state.service}/>
+
+        ) : (
+          null
+        )
+
+        }
         </div>
         <div id="estimateFloatClear"></div>
       </div>
@@ -64,3 +93,16 @@ class EstimateHome extends Component {
 }
  
 export default EstimateHome;
+
+// render() {
+//   const isLoggedIn = this.state.isLoggedIn;
+//   return (
+//     <div>
+//       {isLoggedIn ? (
+//         <LogoutButton onClick={this.handleLogoutClick} />
+//       ) : (
+//         <LoginButton onClick={this.handleLoginClick} />
+//       )}
+//     </div>
+//   );
+// }
