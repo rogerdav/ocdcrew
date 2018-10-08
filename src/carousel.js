62325d2988309adbe74bf1edc7e47a397a58f869
Roger <roger.neil.davenport@gmail.com>
2018-10-08 15:00:05 -0700

    changed some carousel settings

--- src/carousel.js
+++ src/carousel.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default () => (
-  <Carousel autoPlay>
-    {/* <div>
+  <Carousel autoPlay="true" dynamicHeight="true">
+    <div>
       <img src={require('./pics/pic1.jpg')} alt="missing pic one file" width="480px"/>
       <p className="legend">Pic 1</p>
-    </div> */}
+    </div>
     <div>
-      <img src={require('./pics/pic2.jpg')} alt="missing pic one file"  />
+      <img src={require('./pics/test2.gif')} alt="missing pic one file"  />
       <p className="legend">pic 2</p>
     </div>
     <div>
-      <img src={require('./pics/pic3.jpg')} alt="missing pic one file" />
+      <img src={require('./pics/test3.gif')} alt="missing pic one file" />
       <p className="legend">pic3 3</p>
     </div>
     <div>
